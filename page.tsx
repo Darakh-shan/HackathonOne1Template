c87a50c9312cc3661053d7995249ff2beaf828ee
Darakhshan <162590512+Darakh-shan@users.noreply.github.com>
2024-12-18 01:14:47 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,12 +1,21 @@
+
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Brands from './components/Brands'
+import Footer from './components/Footer'
+import Top from './components/Top'
+import Products from './products/page'
+import TopItems from './products/TopItems'
 
 const page = () => {
   return (
-    <main className='flex justify-center'>
-    <div className='font-bold text-4xl flex justify-center pt-24 border-4 border-rose-950 pb-10 w-[30%] hover:border-x-cyan-300'>
-      <h1>Sign Up page</h1>
+    <div>
+      <Hero/>
+      <Brands/>
+      <Products />
+      <TopItems/>
     </div>
-    </main>
   )
 }
 
